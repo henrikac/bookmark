1adf646d4670dce0fad39b0c924fd3194548153d
Henrik Christensen <sensen1695@hotmail.com>
2022-07-18 22:11:54 +0200

    user can now change the path where bookmarks are stored

--- cmd/config.go
+++ cmd/config.go
@@ -18,8 +18,10 @@ package cmd
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -34,6 +36,7 @@ type Config struct {
 var (
 	configCmd     = NewConfigCmd()
 	configListCmd = NewConfigListCmd()
+	configSetCmd  = NewConfigSetCmd()
 )
 
 // NewConfigCmd initializes a new config command.
@@ -67,7 +70,62 @@ func NewConfigListCmd() *cobra.Command {
 	}
 }
 
+func NewConfigSetCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "set",
+		Short: "Sets configuration <config> to the given <value>",
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			config := viper.GetViper().GetString(args[0])
+			if config == "" {
+				return fmt.Errorf("unable to find the given config: \"%s\"", args[0])
+			}
+			switch args[0] {
+			case "storePath":
+				newStorePath := args[1]
+				if !filepath.IsAbs(newStorePath) {
+					absPath, err := filepath.Abs(newStorePath)
+					if err != nil {
+						return err
+					}
+					newStorePath = absPath
+				}
+				dir, file := filepath.Split(newStorePath)
+				if _, err := os.Stat(dir); errors.Is(err, os.ErrNotExist) {
+					err = os.MkdirAll(dir, 0750)
+					if err != nil {
+						return err
+					}
+				}
+				if file == "" {
+					file = "bookmarks.json"
+				}
+				if _, err := os.Stat(config); errors.Is(err, os.ErrNotExist) {
+					b, err := json.Marshal(make(map[string]string))
+					if err != nil {
+						return err
+					}
+					err = os.WriteFile(config, b, 0666)
+					if err != nil {
+						return err
+					}
+				}
+				err := os.Rename(config, filepath.Join(dir, file))
+				if err != nil {
+					return err
+				}
+				viper.GetViper().Set(args[0], newStorePath)
+				return viper.GetViper().WriteConfig()
+			default:
+				return fmt.Errorf("unable to find the given config: \"%s\"", args[0])
+			}
+			return nil
+		},
+	}
+}
+
 func init() {
 	configCmd.AddCommand(configListCmd)
+	configCmd.AddCommand(configSetCmd)
 	rootCmd.AddCommand(configCmd)
 }
